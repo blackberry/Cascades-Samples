e62dd20b2303d7755a8a978be9281454c6fc42d4
Adam Sosnowski <asosnowski@blackberry.com>
2014-04-29 13:28:18 -0400

    added translation files ts

---
+++ IDS_CPP_Sample/translations/IDS_CPP_Sample.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../assets/main.qml" line="33"/>
+        <source>Press Here</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="50"/>
+        <source>Profile results</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
