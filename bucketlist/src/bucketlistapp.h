d94ef6c4daa6a4331def4d766d83ecec3f1b5c94
Oskar Apelvi <oapelvi@blackberry.com>
2014-07-15 13:12:01 +0200

    Changed entry point application code.

--- bucketlist/src/bucketlistapp.h
+++ bucketlist/src/bucketlistapp.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012 BlackBerry Limited.
+/* Copyright (c) 2012, 2013, 2014 BlackBerry Limited.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -76,7 +76,7 @@ public slots:
 
 public:
     // Constructor that sets up the application.
-    BucketListApp();
+    BucketListApp(QObject *parent = 0);
 
 private:
     /**
